08bfb137583723cc3a0b643aa7c7dcac673b5201
Daniel Bednarek <daniel_bednarek@epam.com>
2023-01-11 14:02:03 +0100

    image optimization and home page box setup

--- lib/api.ts
+++ lib/api.ts
@@ -209,6 +209,7 @@ export async function getAllPostsForHome(preview) {
             featuredImage {
               node {
                 sourceUrl
+                altText
               }
             }
             author {
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,46 +5,101 @@ import MoreStories from '../components/more-stories';
 import HeroPost from '../components/hero-post';
 import Intro from '../components/intro';
 import Layout from '../components/layout';
-import { getAllCategories, getMainLogoData, getAllPostsForHome, getAllPostsWithSlug } from '../lib/api';
+import {
+	getAllCategories,
+	getMainLogoData,
+	getAllPostsForHome,
+	getAllPostsWithSlug,
+} from '../lib/api';
 import CategoryPostsBox from '../components/category-posts-box';
 
-export default function Index({ allPostsForHome: { edges }, preview, allCategories, mainLogoData, postsList }: {allPostsForHome?: any, preview: boolean, allCategories?: any, mainLogoData: any, postsList: any}) {
+export default function Index({
+	allPostsForHome: { edges },
+	preview,
+	allCategories,
+	mainLogoData,
+	postsList,
+}: {
+	allPostsForHome?: any;
+	preview: boolean;
+	allCategories?: any;
+	mainLogoData: any;
+	postsList: any;
+}) {
+	const heroPost = edges[0]?.node;
+	const morePostsForHome = edges.slice(1);
+	let moreFilteredPostsForHome;
 
-  const heroPost = edges[0]?.node
-  const morePostsForHome = edges.slice(1)
+	console.log(allCategories);
 
-  return (
-    <Layout preview={preview} allCategories={allCategories} mainLogoData={mainLogoData} postsList={postsList}>
-      <Head>
-        <title>AdsWise | Strona Główna</title>
-      </Head>
-      <Container>
-        <Intro />
-        <CategoryPostsBox category={heroPost.categories}/>
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-            category={heroPost.categories}
-          />
-        )}
-        {morePostsForHome.length > 0 && <MoreStories postsForHome={morePostsForHome} />}
-      </Container>
-    </Layout>
-  )
+	return (
+		<Layout
+			preview={preview}
+			allCategories={allCategories}
+			mainLogoData={mainLogoData}
+			postsList={postsList}
+		>
+			<Head>
+				<title>AdsWise | Strona Główna</title>
+			</Head>
+			<Container>
+				<Intro />
+
+				{heroPost && (
+					<HeroPost
+						title={heroPost.title}
+						coverImage={heroPost.featuredImage}
+						date={heroPost.date}
+						slug={heroPost.slug}
+						excerpt={heroPost.excerpt}
+						category={heroPost.categories}
+					/>
+				)}
+				{/* {morePostsForHome.length > 0 && <MoreStories postsForHome={morePostsForHome} />} */}
+
+				{morePostsForHome.length > 0 &&
+					allCategories.edges.map((category) => {
+						if (!category.node.parent && category.node.slug !== 'pozostale') {
+							moreFilteredPostsForHome = morePostsForHome.filter((post) => {
+								return post.node.categories.nodes[0].slug === category.node.slug;
+							});
+							return (
+								<CategoryPostsBox
+									key={category.node.slug}
+									category={category.node.name}
+									morePostsForHome={moreFilteredPostsForHome}
+								/>
+							);
+						}
+					})}
+				{morePostsForHome.length > 0 &&
+					allCategories.edges.map((category) => {
+						if (!category.node.parent && category.node.slug === 'pozostale') {
+              moreFilteredPostsForHome = morePostsForHome.filter((post) => {
+								return post.node.categories.nodes[0].slug === category.node.slug;
+              });
+							return (
+								<CategoryPostsBox
+									key={category.node.slug}
+									category={category.node.name}
+									morePostsForHome={moreFilteredPostsForHome}
+								/>
+							);
+						}
+					})}
+			</Container>
+		</Layout>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
-  const allPostsForHome = await getAllPostsForHome(preview)
-  const allCategories = await getAllCategories()
-  const mainLogoData = await getMainLogoData()
-  const postsList = await getAllPostsWithSlug();
+	const allPostsForHome = await getAllPostsForHome(preview);
+	const allCategories = await getAllCategories();
+	const mainLogoData = await getMainLogoData();
+	const postsList = await getAllPostsWithSlug();
 
-  return {
-    props: { allPostsForHome, preview, allCategories, mainLogoData, postsList },
-    revalidate: 10,
-  }
-}
+	return {
+		props: { allPostsForHome, preview, allCategories, mainLogoData, postsList },
+		revalidate: 10,
+	};
+};
