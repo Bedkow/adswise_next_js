d54103cd224cc9de1f16750a0fe23cbc050bd428
Daniel Bednarek <daniel_bednarek@epam.com>
2023-01-12 14:55:25 +0100

    routing and breadcrumbs fixes

--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -1,40 +1,63 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-export default function Breadcrumbs({categoriesList, postsList}) {
+export default function Breadcrumbs({ categoriesList, postsList }) {
+	const router = useRouter();
+	const currentQuery = router.query;
 
-    const router = useRouter();
-    const currentQuery = router.query;
+	// const category = categoriesList.find((category) => {
+	//     return category.node.slug === currentQuery.category
+	// })
+	// const categoryName = category?.node.name;
+	// const categorySlug = category?.node.slug;
 
-    const category = categoriesList.find((category) => {
-        return category.node.slug === currentQuery.category
-    })
-    const categoryName = category?.node.name;
-    const categorySlug = category?.node.slug;
+	const post = postsList.edges.find((post) => {
+		return post.node.slug === currentQuery.post;
+	});
 
-    const post = postsList.edges.find((post) => {
-        return post.node.slug === currentQuery.post 
-    })
+	const postTitle = post?.node.title;
+	// console.log(post)
 
-    const postTitle = post?.node.title
+	let postCategoryName;
+	let postCategorySlug;
+	let postCategoryAncestors;
+	let postCategoryAncestorsName;
+	let postCategoryAncestorsSlug;
 
-    let postCategoryName
-    let postCategorySlug 
+	postCategoryName = post?.node.categories.edges[0].node.name;
+	postCategorySlug = post?.node.categories.edges[0].node.slug;
+	postCategoryAncestors = post?.node.categories.edges[0].node.ancestors;
 
-    // if (post.node.categories.edges.length <= 1) {
-    postCategoryName = post?.node.categories.edges[0].node.name;
-    postCategorySlug = post?.node.categories.edges[0].node.slug;
-    // }
+	if (postCategoryAncestors) {
+		postCategoryAncestorsName =
+			post?.node.categories.edges[0].node.ancestors.edges[0].node.name;
+		postCategoryAncestorsSlug =
+			post?.node.categories.edges[0].node.ancestors.edges[0].node.slug;
+	}
 
-    return (
-        <>
-            <div>
-                {postCategoryName && <Link href='/'><span>Strona Główna</span></Link>}
-                {postCategoryName && <span>{` > `}</span>}
-                {postCategoryName && <Link href={`/${postCategorySlug}`}><span>{postCategoryName}</span></Link>}
-                {postTitle && <span>{` > `}</span>}
-                {postTitle && <span>{postTitle}</span>}
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div>
+				{postCategoryName && (
+					<Link href="/">
+						<span>Strona Główna</span>
+					</Link>
+				)}
+				{postCategoryAncestors && <span>{` > `}</span>}
+				{postCategoryAncestors && (
+					<Link href={`/${postCategoryAncestorsSlug}`}>
+						<span>{postCategoryAncestorsName}</span>
+					</Link>
+				)}
+				{postCategoryName && <span>{` > `}</span>}
+				{postCategoryName && (
+					<Link href={`/${postCategorySlug}`}>
+						<span>{postCategoryName}</span>
+					</Link>
+				)}
+				{postTitle && <span>{` > `}</span>}
+				{postTitle && <span>{postTitle}</span>}
+			</div>
+		</>
+	);
 }
