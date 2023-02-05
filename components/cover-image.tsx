a38bd509b6b78075cbd0a72e51d0c36bb43c8957
Bedkow <bednarekdaniel@o2.pl>
2023-02-05 21:57:39 +0100

    add cond rendering on basis of cat.posts.length > 0 in category buttons and tiles rendering

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,52 +1,66 @@
-
-import Image from 'next/image'
-import Link from 'next/link'
-import styled from 'styled-components'
+// import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
 
 const CoverImageContainer = styled.div`
-  /* height: 400px; */
-  width: 100%;
-  position: relative;
 
-  Image {
-    object-fit: contain;
-    height: 100%;
-    width: auto;
-    max-width: 100%;
+	width: 100%;
+
+	img {
+		height: auto;
+		max-width: 100%;
+	}
+
+  .image-container {
+    margin: 30px auto;
+    width: 100%;
+    max-width: 2000px;
   }
-`
+
+`;
 
 interface Props {
-  title: string
-  coverImage: {
-    node: {
-      sourceUrl: string
-    }
-  }
-  slug?: string
-  category?: string
+	title: string;
+	coverImage: {
+		node: {
+			sourceUrl: string;
+		};
+	};
+	slug?: string;
+	category?: string;
 }
 
-export default function CoverImage({ title, coverImage, slug, category }: Props) {
-  const image = (
-    <Image
-      width={960}
-      height={540}
-      priority
-      alt={`Cover Image for ${title}`}
-      src={coverImage?.node.sourceUrl}
-      
-    />
-  )
-  return (
-    <CoverImageContainer>
-      {slug && coverImage ? (
-        <Link href={`/post/${slug}`} aria-label={title}>
-          {image}
-        </Link>
-      ) : (
-        null
-      )}
-    </CoverImageContainer>
-  );
+export default function CoverImage({
+	title,
+	coverImage,
+	slug,
+	category,
+}: Props) {
+	// const image = (
+	//   <Image
+	//     // width={960}
+	//     // height={540}
+	//     priority
+	//     fill
+	//     alt={`Cover Image for ${title}`}
+	//     src={coverImage?.node.sourceUrl}
+
+	//   />
+	// )
+	return (
+		<CoverImageContainer>
+			{slug && coverImage ? (
+        <div className='image-container'>
+				<Link href={`/post/${slug}`} aria-label={title}>
+					
+						<img
+							alt={`Cover Image for ${title}`}
+							src={coverImage?.node.sourceUrl}
+						/>
+					
+				</Link>
+        </div>
+			) : null}
+		</CoverImageContainer>
+	);
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,44 +1,47 @@
-import Link from 'next/link';
-import MainLogo from './main-logo';
-import Breadcrumbs from './breadcrumbs';
-import styled from 'styled-components'
+import Link from "next/link";
+import MainLogo from "./main-logo";
+import Breadcrumbs from "./breadcrumbs";
+import styled from "styled-components";
 
 const MainNav = styled.nav`
-  width: 100%;
-`
+	width: 100%;
+`;
 
 const CategoryList = styled.ul`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  list-style-type: none;
-  justify-content: center;
-`
+	padding: 20px 0;
+	width: 100%;
+	display: flex;
+	gap: 10px;
+	flex-direction: row;
+	list-style-type: none;
+	justify-content: center;
+	flex-wrap: wrap;
+`;
 
 const CategoryListItem = styled.li`
-  border: 1px solid black;
-  padding: 5px;
-`
+	border: 1px solid black;
+	padding: 7px;
+	border-radius: 5px;
+`;
 
 const SubcategoryList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  list-style-type: none;
-  display: none;
-  z-index: 10;
+	display: flex;
+	flex-direction: column;
+	list-style-type: none;
+	display: none;
+	z-index: 10;
 
-  ${CategoryListItem}:hover & {
-    display: flex;
-    position: absolute;
-  }
-`
+	${CategoryListItem}:hover & {
+		display: flex;
+		position: absolute;
+	}
+`;
 
 const SubcategoryListItem = styled.li`
-  border: 1px dotted black;
-  padding: 3px;
-  background-color: white;
-`
-
+	border: 1px dotted black;
+	padding: 3px;
+	background-color: white;
+`;
 
 export default function Header({ allCategories, mainLogoData, postsList }) {
 	const categoriesList = allCategories?.edges;
@@ -48,39 +51,49 @@ export default function Header({ allCategories, mainLogoData, postsList }) {
 			<MainLogo mainLogoData={mainLogoData} />
 			<MainNav>
 				<CategoryList>
-					{/* <CategoryListItem key={'strona-glowna'}>
-						<Link href="/">Strona Główna</Link>
-					</CategoryListItem> */}
 					{categoriesList?.map((category) => {
 						if (
-							category.node.slug !== 'pozostale' &&
-							category.node.parent === null
+							category.node.slug !== "pozostale" &&
+							category.node.parent === null &&
+							category.node.contentNodes.nodes.length > 0
 						) {
 							return (
 								<CategoryListItem key={category.node.slug}>
 									<Link href={`/${category.node.slug}`}>
 										{category.node.name}
 									</Link>
-                  {category.node.children.nodes.length > 0 && <SubcategoryList>
-                    {category.node.children.nodes.map(
-                      (subCategory) => {
-                        return (
-                          <SubcategoryListItem key={`${subCategory.slug}`}>
-                            <Link href={`/${subCategory.slug}`}>subCat - {subCategory.name}</Link>
-                          </SubcategoryListItem>
-                        )
-                      }
-                    )}
-                  </SubcategoryList>}
+									{category.node.children.nodes.length > 0 && (
+										<SubcategoryList>
+											{category.node.children.nodes.map((subCategory) => {
+												return (
+													<SubcategoryListItem key={`${subCategory.slug}`}>
+														<Link href={`/${subCategory.slug}`}>
+															subCat - {subCategory.name}
+														</Link>
+													</SubcategoryListItem>
+												);
+											})}
+										</SubcategoryList>
+									)}
 								</CategoryListItem>
 							);
 						}
 					})}
-					<CategoryListItem>
-						<Link key="pozostale" href="/pozostale">
-							Pozostałe
-						</Link>
-					</CategoryListItem>
+					{categoriesList?.map((category) => {
+						if (
+							category.node.slug === "pozostale" &&
+							category.node.parent === null &&
+							category.node.contentNodes.nodes.length > 0
+						) {
+							return (
+								<CategoryListItem>
+									<Link key='pozostale' href='/pozostale'>
+										Pozostałe
+									</Link>
+								</CategoryListItem>
+							)
+						}
+					})}
 				</CategoryList>
 			</MainNav>
 			<Breadcrumbs categoriesList={categoriesList} postsList={postsList} />
