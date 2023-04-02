7f675f0f95f8633c20471cc5f7510130441f774d
Bedkow <bednarekdaniel@o2.pl>
2023-04-02 23:41:58 +0200

    comment logo out for now

--- components/main-logo.tsx
+++ components/main-logo.tsx
@@ -8,12 +8,12 @@ const LogoContainer = styled.div`
 `
 
 export default function MainLogo({mainLogoData}) {
- const mainLogoDataTest = mainLogoData?.edges[0].node;
-console.log(mainLogoDataTest.sourceUrl)
+//  const mainLogoDataTest = mainLogoData?.edges[0].node;
+// console.log(mainLogoDataTest.sourceUrl)
     return (
         <LogoContainer>
         <Link href={'/'}>
-        <Image src={mainLogoDataTest.sourceUrl}  alt={mainLogoDataTest.altText} width={279/2} height={136/2} />
+        {/* <Image src={mainLogoDataTest.sourceUrl}  alt={mainLogoDataTest.altText} width={279/2} height={136/2} /> */}
         </Link>
         </LogoContainer>
     )
