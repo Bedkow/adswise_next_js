a68ed35211066914d3242ac038d8ebeb783802a8
Bedkow <bednarekdaniel@o2.pl>
2023-06-28 22:59:46 +0200

    some styling

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { paginate } from "../helpers/paginate";
 import styled from 'styled-components';
 
+// const StyledPostPreviewContainer
+
 export default function MoreStories({
 	postsForHome,
 	postsForReadMore,
