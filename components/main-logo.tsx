6f8011b7fa96dcde6d9347aac34b010098b6886d
Bedkow <bednarekdaniel@o2.pl>
2023-07-09 13:19:21 +0200

    add plain text logo and position it

--- components/main-logo.tsx
+++ components/main-logo.tsx
@@ -7,12 +7,26 @@ const LogoContainer = styled.div`
     display: inline-block;
 `
 
+const PlainTextLogo = styled.span`
+    font-size: 32px;
+    height: 80px;
+    width: min-content;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    left: 20px;
+`
+
 export default function MainLogo({mainLogoData}) {
 //  const mainLogoDataTest = mainLogoData?.edges[0].node;
 // console.log(mainLogoDataTest.sourceUrl)
     return (
         <LogoContainer>
-        <Link href={'/'}>
+        <Link href={'/'} style={{
+            textDecoration: 'none',
+        }}>
+            <PlainTextLogo>AdsWise</PlainTextLogo>
         {/* <Image src={mainLogoDataTest.sourceUrl}  alt={mainLogoDataTest.altText} width={279/2} height={136/2} /> */}
         </Link>
         </LogoContainer>
