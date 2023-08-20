d1dcae8db6f353cc8790cffb5c967115788b1f71
Bedkow <bednarekdaniel@o2.pl>
2023-08-20 20:04:44 +0200

    subpages styling and breadcrumb fixes

--- pages/kontakt/index.tsx
+++ pages/kontakt/index.tsx
@@ -1,11 +1,26 @@
 import Layout from "../../components/layout";
 import { GetStaticProps } from "next";
 import { getMainLogoData } from "../../lib/api";
+import styled from "styled-components";
+
+const StyledTitle = styled.h1`
+  padding: 5px;
+  width: min-content;
+  border: 3px black solid;
+  margin-bottom: 50px;
+`
+
+const StyledEmail = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
+`
 
 export default function Contact({ mainLogoData }) {
 	return (
 		<Layout preview={false} mainLogoData={mainLogoData}>
-			<div>Kontakt</div>
+			<StyledTitle>Kontakt</StyledTitle>
+      <div>Wszelkie zapytania prosimy kierować na adres e-mail: <StyledEmail href="mailto:kontakt@adswise.pl">kontakt@adswise.pl</StyledEmail></div>
 		</Layout>
 	);
 }
