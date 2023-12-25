e93627061febe9d0d9b2137484c7ea411c259280
Bedkow <bednarekdaniel@o2.pl>
2023-12-25 19:17:10 +0100

    disable robots in head

--- components/meta.tsx
+++ components/meta.tsx
@@ -37,6 +37,7 @@ export default function Meta() {
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta name="robots" content="noindex, nofollow" />  {/* TODO: delete to allow bots*/}
     </Head>
   )
 }
